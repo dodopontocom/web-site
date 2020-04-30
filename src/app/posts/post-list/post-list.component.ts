9751751f2b1f2aefc5c42810fcd9961000345720
rodolfotiago <rodolfotiago@gmail.com>
2020-04-30 12:06:58 -0300

    panel list testing

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { style } from '@angular/animations';
+
+@Component ({
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent{}
