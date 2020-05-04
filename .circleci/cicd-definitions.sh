2f20cb98b1dd1f588af81fdeff405b5936ae8f7d
rodolfotiago <rodolfotiago@gmail.com>
2020-05-04 17:16:32 -0300

    check

--- .circleci/cicd-definitions.sh
+++ .circleci/cicd-definitions.sh
@@ -19,4 +19,5 @@ export TF_VAR_key="${CIRCLE_WORKING_DIRECTORY}/cloud/credentials/credential.json
 export GOOGLE_APPLICATION_CREDENTIALS=${TF_VAR_key}
 
 # Telegram notification via bot
-declare -a NOTIFICATION_IDS=(11504381)
+export NOTIFICATION_IDS=(11504381)
+declare -p NOTIFICATION_IDS
