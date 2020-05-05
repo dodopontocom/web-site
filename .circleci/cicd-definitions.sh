edf26d7263bd185708f5c3b91ae8327eafbf751e
rodolfotiago <rodolfotiago@gmail.com>
2020-05-05 16:01:49 -0300

    [tf-apply] agora vai

--- .circleci/cicd-definitions.sh
+++ .circleci/cicd-definitions.sh
@@ -11,6 +11,7 @@ export GCLOUD_PROJECT_REGION="us-central1"
 
 DOCKER_REGISTRY_SECRET_NAME="gcr-secret"
 GCLOUD_SA_NAME="devops-sa"
+GCLOUD_SA_EMAIL="${GCLOUD_SA_NAME}@${GCLOUD_PROJECT_ID}.iam.gserviceaccount.com"
 
 # Terraform variables
 export TF_VAR_gcp_bucket="${GCLOUD_PROJECT_BUCKET_NAME}"
