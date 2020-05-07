85e6250b732abac97f3e06eec062643b114ac540
rodolfotiago <rodolfotiago@gmail.com>
2020-05-07 09:21:37 -0300

    testing leos dolibs

---
+++ dolibs-test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# This line of code will source the dolibs entrypoint, this format help to find dolibs.sh 
+# file relative to the current script path, regardesless of where it was executed
+source $(dirname ${BASH_SOURCE[0]})/dolibs.sh
+
+# Import required lib
+do.use gcp
+
+# Use the lib
+gcp.useSA ${GOOGLE_APPLICATION_CREDENTIALS}
