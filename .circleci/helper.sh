dcc6750fb3abe61c497e0719c5710241f5f88fb8
rodolfotiago <rodolfotiago@gmail.com>
2020-05-07 19:38:42 -0300

    testing

---
+++ .circleci/helper.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+source $(dirname ${BASH_SOURCE[0]})/../dolibs.sh
+source $(dirname ${BASH_SOURCE[0]})/cicd-definitions.sh
+
+echo ${DODRONES_GCP_MY_LABS_SA} > ${GCLOUD_JSON_KEY_PATH}
+
+# Import required lib
+do.use gcp.auth
+do.use gcp.gcr
+
+# Use the lib
+gcp.auth.useSA ${GOOGLE_APPLICATION_CREDENTIALS}
+gcp.gcr.dockerLogin ${GOOGLE_APPLICATION_CREDENTIALS}
