e49fd5d3ceb3b3e9b3e3601186ae54f13e2cbe56
rodolfotiago <rodolfotiago@gmail.com>
2020-05-07 10:19:52 -0300

    source

--- dolibs-test.sh
+++ dolibs-test.sh
@@ -3,9 +3,10 @@
 # This line of code will source the dolibs entrypoint, this format help to find dolibs.sh 
 # file relative to the current script path, regardesless of where it was executed
 source $(dirname ${BASH_SOURCE[0]})/dolibs.sh
+source $(dirname ${BASH_SOURCE[0]})/.circleci/cicd-definitions.sh
 
 # Import required lib
-do.import gcp
+do.use gcp
 
 # Use the lib
 gcp.auth.useSA ${GOOGLE_APPLICATION_CREDENTIALS}
