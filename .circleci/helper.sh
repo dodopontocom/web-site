45f11c8b6709ec829f233af26914430e5fcc8eb2
rodolfotiago <rodolfotiago@gmail.com>
2020-05-07 20:48:43 -0300

    ok

--- .circleci/helper.sh
+++ .circleci/helper.sh
@@ -8,7 +8,9 @@ echo ${DODRONES_GCP_MY_LABS_SA} > ${GCLOUD_JSON_KEY_PATH}
 # Import required lib
 #do.use gcp.auth
 do.use gcp.gcr
+do.use gcp.gcs
 
 # Use the lib
-#gcp.auth.useSA ${GOOGLE_APPLICATION_CREDENTIALS}
-gcp.gcr.dockerLogin ${GOOGLE_APPLICATION_CREDENTIALS}
+gcp.auth.useSA ${GOOGLE_APPLICATION_CREDENTIALS}
+gcp.gcs.validateBucket ${GCLOUD_PROJECT_ID} ${GCLOUD_PROJECT_BUCKET_NAME} 
+#gcp.gcr.dockerLogin ${GOOGLE_APPLICATION_CREDENTIALS}
