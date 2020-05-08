063e18b567dc9454cd82082b360dbbf98ae1b2d5
rodolfotiago <rodolfotiago@gmail.com>
2020-05-07 21:34:27 -0300

    more cluster

--- .circleci/helper.sh
+++ .circleci/helper.sh
@@ -7,10 +7,11 @@ echo ${DODRONES_GCP_MY_LABS_SA} > ${GCLOUD_JSON_KEY_PATH}
 
 # Import required lib
 do.use gcp.auth
-#do.use gcp.gcr
 do.use gcp.gcs
+do.use gcp.gke
 
 # Use the lib
 gcp.auth.useSA ${GOOGLE_APPLICATION_CREDENTIALS}
-gcp.gcs.createBucket ${GCLOUD_PROJECT_ID} los_hermanos regional ${GCLOUD_PROJECT_REGION}
-gcp.gcs.validateBucket ${GCLOUD_PROJECT_ID} los_hermanos
+gcp.gcs.validateBucket ${GCLOUD_PROJECT_ID} ${GCLOUD_PROJECT_BUCKET_NAME}
+gcp.gke.describeCluster ${TF_VAR_cluster_name} ${TF_VAR_zone} ${GCLOUD_PROJECT_ID}
+gcp.gke.loginCluster ${TF_VAR_cluster_name} ${TF_VAR_zone} ${GCLOUD_PROJECT_ID}
