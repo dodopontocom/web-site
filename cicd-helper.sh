f8c384c65b33de3f8ed2f31a4c9dd256372b128e
rodolfotiago <rodolfotiago@gmail.com>
2020-05-07 23:04:10 -0300

    testing

--- cicd-helper.sh
+++ cicd-helper.sh
@@ -5,24 +5,27 @@ which terraform && apk add --no-cache curl
 source $(dirname ${BASH_SOURCE[0]})/dolibs.sh
 source $(dirname ${BASH_SOURCE[0]})/.circleci/cicd-definitions.sh
 
-echo ${DODRONES_GCP_MY_LABS_SA} > ${GCLOUD_JSON_KEY_PATH}
+# Execute Depending on the CircleCi Job(step)
+if [[ ${CIRCLE_JOB} -eq "GCP GKE Provisioning" ]]; then
 
-# Import required lib
-do.use terraform
+    terraform_path="cloud/terraform"
+    # Import required lib
+    do.use terraform
+
+    # Terraform Provisioning steps
+    echo ${DODRONES_GCP_MY_LABS_SA} > ${TF_VAR_key}
+    cd ${terraform_path}
+    terraform init -backend-config="bucket=${TF_VAR_gcp_bucket}" -backend-config="prefix=terraform"
+    #terraform destroy --auto-approve
+    terraform.init "${terraform_path}"
+    terraform.apply "${terraform_path} quiet=true"
+fi
+
+# GCP steps
+#echo ${DODRONES_GCP_MY_LABS_SA} > ${GCLOUD_JSON_KEY_PATH}
 #do.use gcp.auth
 #do.use gcp.gcs
 #do.use gcp.gke
-
-# Terraform Provisioning steps
-#terraform.createBackEndGCP "cloud/terraform" ${GCLOUD_PROJECT_BUCKET_NAME} "terraform"
-echo ${DODRONES_GCP_MY_LABS_SA} > ${TF_VAR_key}
-cd cloud/terraform
-terraform init -backend-config="bucket=${TF_VAR_gcp_bucket}" -backend-config="prefix=terraform"
-#terraform destroy --auto-approve
-terraform.init "cloud/terraform"
-terraform.apply "cloud/terraform quiet=true"
-
-# GCP steps
 #gcp.auth.useSA ${GOOGLE_APPLICATION_CREDENTIALS}
 #gcp.gcs.validateBucket ${GCLOUD_PROJECT_ID} ${GCLOUD_PROJECT_BUCKET_NAME}
 #gcp.gke.describeCluster ${TF_VAR_cluster_name} ${TF_VAR_zone} ${GCLOUD_PROJECT_ID}
