4f3f95a61a05541baa1e69d93ba43e716db2517f
rodolfotiago <rodolfotiago@gmail.com>
2020-05-08 14:13:34 -0300

    fix

--- .circleci/cicd-executor.sh
+++ .circleci/cicd-executor.sh
@@ -4,7 +4,7 @@ if [[ ${CI} ]]; then
     which apk && apk add --no-cache curl jq
 fi
 
-ROOT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}")/ >/dev/null 2>&1 && pwd)
+ROOT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}")../ >/dev/null 2>&1 && pwd)
 #ROOT_DIR="$(dirname ${BASH_SOURCE[0]})/.."
 source ${ROOT_DIR}/dolibs.sh
 source ${ROOT_DIR}/.circleci/cicd-definitions.sh
