705607a127235b05829cbc1de1b92590c3c2060d
rodolfotiago <rodolfotiago@gmail.com>
2020-05-08 14:15:39 -0300

    fix path

--- .circleci/cicd-executor.sh
+++ .circleci/cicd-executor.sh
@@ -4,12 +4,8 @@ if [[ ${CI} ]]; then
     which apk && apk add --no-cache curl jq
 fi
 
-ROOT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}")../ >/dev/null 2>&1 && pwd)
-#ROOT_DIR="$(dirname ${BASH_SOURCE[0]})/.."
-source ${ROOT_DIR}/dolibs.sh
-source ${ROOT_DIR}/.circleci/cicd-definitions.sh
-#source $(dirname ${BASH_SOURCE[0]})/../dolibs.sh
-#source $(dirname ${BASH_SOURCE[0]})/cicd-definitions.sh
+source $(dirname ${BASH_SOURCE[0]})/../dolibs.sh
+source $(dirname ${BASH_SOURCE[0]})/cicd-definitions.sh
 
 # Use Telegram lib for sending after notifications
 do.use telegram
@@ -20,7 +16,7 @@ if [[ "${CIRCLE_JOB}" == "GCP GKE Provisioning" ]]; then
     # Import required lib
     do.use terraform
 
-    terraform_path="${ROOT_DIR}/cloud/terraform"
+    terraform_path="../cloud/terraform"
     echo ${DODRONES_GCP_MY_LABS_SA} > ${TF_VAR_key}
     
     cd ${terraform_path}
