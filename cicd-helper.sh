29aad280cb9eb79fcfb0ed3735e37d500c035cf2
rodolfotiago <rodolfotiago@gmail.com>
2020-05-07 23:06:39 -0300

    oi

--- cicd-helper.sh
+++ cicd-helper.sh
@@ -6,7 +6,7 @@ source $(dirname ${BASH_SOURCE[0]})/dolibs.sh
 source $(dirname ${BASH_SOURCE[0]})/.circleci/cicd-definitions.sh
 
 # Execute Depending on the CircleCi Job(step)
-if [[ ${CIRCLE_JOB} -eq "GCP GKE Provisioning" ]]; then
+if [[ "${CIRCLE_JOB}" -eq "GCP GKE Provisioning" ]]; then
 
     terraform_path="cloud/terraform"
     # Import required lib
@@ -20,13 +20,14 @@ if [[ ${CIRCLE_JOB} -eq "GCP GKE Provisioning" ]]; then
     terraform.init "${terraform_path}"
     terraform.apply "${terraform_path} quiet=true"
 fi
-
-# GCP steps
-#echo ${DODRONES_GCP_MY_LABS_SA} > ${GCLOUD_JSON_KEY_PATH}
-#do.use gcp.auth
-#do.use gcp.gcs
-#do.use gcp.gke
-#gcp.auth.useSA ${GOOGLE_APPLICATION_CREDENTIALS}
-#gcp.gcs.validateBucket ${GCLOUD_PROJECT_ID} ${GCLOUD_PROJECT_BUCKET_NAME}
-#gcp.gke.describeCluster ${TF_VAR_cluster_name} ${TF_VAR_zone} ${GCLOUD_PROJECT_ID}
-#gcp.gke.loginCluster ${TF_VAR_cluster_name} ${TF_VAR_zone} ${GCLOUD_PROJECT_ID}
+if [[ "${CIRCLE_JOB}" -eq "GCP Deploy App" ]]; then
+    # GCP steps
+    #echo ${DODRONES_GCP_MY_LABS_SA} > ${GCLOUD_JSON_KEY_PATH}
+    #do.use gcp.auth
+    #do.use gcp.gcs
+    #do.use gcp.gke
+    #gcp.auth.useSA ${GOOGLE_APPLICATION_CREDENTIALS}
+    #gcp.gcs.validateBucket ${GCLOUD_PROJECT_ID} ${GCLOUD_PROJECT_BUCKET_NAME}
+    #gcp.gke.describeCluster ${TF_VAR_cluster_name} ${TF_VAR_zone} ${GCLOUD_PROJECT_ID}
+    #gcp.gke.loginCluster ${TF_VAR_cluster_name} ${TF_VAR_zone} ${GCLOUD_PROJECT_ID}
+fi
