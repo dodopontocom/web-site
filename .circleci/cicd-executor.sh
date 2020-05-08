1a1cdfb413db44bf08f0c21a68b021850188f3fa
rodolfotiago <rodolfotiago@gmail.com>
2020-05-08 14:03:45 -0300

    path

--- .circleci/cicd-executor.sh
+++ .circleci/cicd-executor.sh
@@ -4,29 +4,25 @@ if [[ ${CI} ]]; then
     which apk && apk add --no-cache curl jq
 fi
 
+#ROOT_DIR="$(dirname ${BASH_SOURCE[0]})/.."
+#source ${ROOT_DIR}/dolibs.sh
+#source ${ROOT_DIR}/.circleci/cicd-definitions.sh
 source $(dirname ${BASH_SOURCE[0]})/../dolibs.sh
 source $(dirname ${BASH_SOURCE[0]})/cicd-definitions.sh
-
-# Use telegram lib
-do.use telegram
-
-# Send a message via bot
-telegram.validateToken ${TELEGRAM_BOT_TOKEN}
-telegram.sendMessage ${TELEGRAM_BOT_TOKEN} ${TELEGRAM_NOTIFICATION_ID} "Testing from circle ci job: ${CIRCLE_JOB}"
 
 # Execute Depending on the CircleCi Job(step)
 if [[ "${CIRCLE_JOB}" == "GCP GKE Provisioning" ]]; then
 
     # Import required lib
     do.use terraform
-
+    terraform_path="cloud/terraform"
     echo ${DODRONES_GCP_MY_LABS_SA} > ${TF_VAR_key}
     
     cd ${terraform_path}
     #terraform init -backend-config="bucket=${TF_VAR_gcp_bucket}" -backend-config="prefix=terraform"
     #terraform destroy --auto-approve
     echoInfo "Initializing Terraform with GCP backEnd ${GCLOUD_PROJECT_BUCKET_NAME}"
-    terraform.init "cloud/terraform" "${GCLOUD_PROJECT_BUCKET_NAME}" "terraform"
+    terraform.init "${terraform_path}" "${GCLOUD_PROJECT_BUCKET_NAME}" "terraform"
     #terraform.apply "${terraform_path}"
 fi
 # if [[ "${CIRCLE_JOB}" == "GCP Deploy App" ]]; then
@@ -43,3 +39,11 @@ fi
 #     gcp.gke.describeCluster ${TF_VAR_cluster_name} ${TF_VAR_zone} ${GCLOUD_PROJECT_ID}
 #     gcp.gke.loginCluster ${TF_VAR_cluster_name} ${TF_VAR_zone} ${GCLOUD_PROJECT_ID}
 # fi
+
+
+# AFTER JOBS - SEND NOTIFICATIONS
+
+do.use telegram
+
+telegram.validateToken ${TELEGRAM_BOT_TOKEN}
+telegram.sendMessage ${TELEGRAM_BOT_TOKEN} ${TELEGRAM_NOTIFICATION_ID} "Testing from circle ci job: ${CIRCLE_JOB}"
