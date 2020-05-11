c379a16b452355bde4722b3b5df84abb7c6a6cdb
rodolfotiago <rodolfotiago@gmail.com>
2020-05-11 13:03:30 -0300

    testing all

--- .circleci/executors/app-deploy.sh
+++ .circleci/executors/app-deploy.sh
@@ -1,5 +1,48 @@
 #!/usr/bin/env bash
 #
 executor.deploy_app() {
-    echoInfo "worked!"
+
+    echo ${DODRONES_GCP_MY_LABS_SA} > ${GCLOUD_JSON_KEY_PATH}
+    
+    # Import required lib
+    do.use gcp.auth
+    do.use gcp.gke
+    do.use k8s
+    
+    if [[ "$(git log --format=oneline -n 1 ${CIRCLE_SHA1} | grep -E "\[tf-destroy\]")" ]]; then
+    
+        echoInfo "Skipping this step... flag 'destroy' is set"
+        integrations.telegram.sendMessage "${TELEGRAM_NOTIFICATION_ID}" "Application deployment was skipped on job: ${CIRCLE_JOB}"
+        integrations.slack.sendMessageToChannel "bashlibs" "Application deployment was skipped on job: ${CIRCLE_JOB}"
+
+    elif [[ "$(git log --format=oneline -n 1 ${CIRCLE_SHA1} | grep -E "\[tf-apply\]")" ]]; then
+
+        echoInfo "Deploying Application to Kubernetes Cluster"
+        gcp.auth.useSA ${GOOGLE_APPLICATION_CREDENTIALS}
+        gcp.gke.loginCluster ${TF_VAR_cluster_name} ${TF_VAR_zone} ${GCLOUD_PROJECT_ID}
+
+        k8s.deployYaml "${ROOT_DIR}/cloud/k8s/app-deployment.yaml"
+        k8s.deployYaml "${ROOT_DIR}/cloud/k8s/app-load-balancer-service.yaml"
+
+        while [ "$(kubectl get services -l label-key='deployment-dev' -o jsonpath='{.items[0].status.loadBalancer.ingress[0].ip}')" == "" ]; do
+            echoInfo "[$(date +%H:%M:%S)] - Getting External IP... [pending]"
+            sleep 10
+        done
+
+        echoInfo "[$(date +%H:%M:%S)] - Getting External IP... [success]"
+        echoInfo "Access the below URL to test your deployed application:"
+        app_url="$(kubectl get services -l label-key='deployment-dev' -o jsonpath='{.items[0].status.loadBalancer.ingress[0].ip}')"
+        echoInfo "http://${app_url}"
+
+        integrations.telegram.sendMessage "${TELEGRAM_NOTIFICATION_ID}" "Application deployment done on job: ${CIRCLE_JOB}"
+        integrations.telegram.sendMessage "${TELEGRAM_NOTIFICATION_ID}" "You can access the App here: ${app_url}"
+        integrations.slack.sendMessageToChannel "bashlibs" "Application deployment done on job: ${CIRCLE_JOB}"
+        integrations.slack.sendMessageToChannel "bashlibs" "You can access the App here: ${app_url}"
+    
+    else
+    
+        echoInfo "Skipping this step... no flag is set"
+        integrations.telegram.sendMessage "${TELEGRAM_NOTIFICATION_ID}" "Application deployment was skipped on job: ${CIRCLE_JOB}"
+    
+    fi
 }
--- .circleci/executors/gke-provisioning.sh
+++ .circleci/executors/gke-provisioning.sh
@@ -1,5 +1,36 @@
 #!/usr/bin/env bash
 #
 executor.gke_provisioning() {
-    echoInfo "worked!"
+
+    # Import required lib
+    do.use terraform
+
+    terraform_path="${ROOT_DIR}/cloud/terraform"
+    echo ${DODRONES_GCP_MY_LABS_SA} > ${TF_VAR_key}
+
+    if [[ "$(git log --format=oneline -n 1 ${CIRCLE_SHA1} | grep -E "\[tf-destroy\]")" ]]; then
+    
+        echoInfo "Terraform destroy flag detected! [Destroying GCP Resources]"
+        terraform.init_gcp "${terraform_path}" "${GCLOUD_PROJECT_BUCKET_NAME}" "terraform"
+        terraform destroy --auto-approve
+
+        integrations.telegram.sendMessage "${TELEGRAM_NOTIFICATION_ID}" "Terraform destroy successfully executed on job: ${CIRCLE_JOB}"
+        integrations.slack.sendMessageToChannel "bashlibs" "Terraform destroy successfully executed on job: ${CIRCLE_JOB}"
+
+    elif [[ "$(git log --format=oneline -n 1 ${CIRCLE_SHA1} | grep -E "\[tf-apply\]")" ]]; then
+    
+        echoInfo "Terraform Apply flag detected!... [Updating GCP Resources]"
+        terraform.init_gcp "${terraform_path}" "${GCLOUD_PROJECT_BUCKET_NAME}" "terraform"
+        terraform.apply "${terraform_path}"
+
+        integrations.telegram.sendMessage "${TELEGRAM_NOTIFICATION_ID}" "Terraform apply successfully executed on job: ${CIRCLE_JOB}"
+        integrations.slack.sendMessageToChannel "bashlibs" "Terraform apply successfully executed on job: ${CIRCLE_JOB}"
+    
+    else
+    
+        echoInfo "Terraform will not be executed!"
+        integrations.telegram.sendMessage "${TELEGRAM_NOTIFICATION_ID}" "Terraform was not executed on job: ${CIRCLE_JOB}"
+        integrations.slack.sendMessageToChannel "bashlibs" "Terraform was not executed on job: ${CIRCLE_JOB}"
+    
+    fi
 }
