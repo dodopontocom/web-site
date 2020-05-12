fa2fb0a2e1cd449cd9b69edf946a5345092851d9
rodolfotiago <rodolfotiago@gmail.com>
2020-05-11 21:51:56 -0300

    fix dev

---
+++ .circleci/cicd-definitions.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+
+# APPLICATION INFO
+
+# GCLOUD ENV VARS
+export GCLOUD_PROJECT_ID="xxx"
+export GCLOUD_JSON_KEY_PATH="${CIRCLE_WORKING_DIRECTORY}/cloud/credentials/credential.json"
+export GCLOUD_PROJECT_BUCKET_NAME="terraform-gke-node"
+export GCLOUD_PROJECT_REGION="us-central1"
+
+export DOCKER_REGISTRY_SECRET_NAME="gcr-secret"
+export GCLOUD_SA_NAME="devops-sa"
+export GCLOUD_SA_EMAIL="${GCLOUD_SA_NAME}@${GCLOUD_PROJECT_ID}.iam.gserviceaccount.com"
+export GCLOUD_CR_BUCKET="us.artifacts.${GCLOUD_PROJECT_ID}.appspot.com"
+
+# Terraform variables
+export TF_VAR_gcp_bucket="${GCLOUD_PROJECT_BUCKET_NAME}"
+export TF_VAR_project_id="${GCLOUD_PROJECT_ID}"
+export TF_VAR_machine_type="n1-standard-4"
+export TF_VAR_region="${GCLOUD_PROJECT_REGION}"
+export TF_VAR_zone="${GCLOUD_PROJECT_REGION}-a"
+export TF_VAR_cluster_name="gke-cluster-1"
+export TF_VAR_cluster_count="3"
+export TF_VAR_key="${GCLOUD_JSON_KEY_PATH}"
+
+export GOOGLE_APPLICATION_CREDENTIALS=${TF_VAR_key}
+
+# Circle ci references
+export CIRCLE_COMMIT_APPLY="tf-apply"
+export CIRCLE_COMMIT_DESTROY="tf-destroy"
+export CIRCLE_COMMIT_SKIP_DOCKER="skip-docker"
---
+++ .circleci/executors/gke-provisioning.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#
+executor.GCP_GKE_Provisioning() {
+
+    local loc destroy
+    loc=$1
+    destroy=$2
+
+    # Import required lib
+    do.use terraform
+
+    terraform_path="${ROOT_DIR}/cloud/terraform"
+    TF_VAR_key=${GOOGLE_APPLICATION_CREDENTIALS}
+    checkVars loc || echo ${DODRONES_GCP_MY_LABS_SA} > ${TF_VAR_key}
+
+    if [[ "$(git log --format=oneline -n 1 ${CIRCLE_SHA1} | grep -E "\[${CIRCLE_COMMIT_DESTROY}\]")" ]] \
+        || [[ -n ${destroy} ]]; then
+    
+        echoInfo "Terraform destroy flag detected! [Destroying GCP Resources]"
+        terraform.init_gcp "${terraform_path}" "${GCLOUD_PROJECT_BUCKET_NAME}" "terraform"
+        terraform destroy --auto-approve
+
+        integrations.telegram.sendMessage "${TELEGRAM_NOTIFICATION_ID}" "Terraform destroy successfully executed on job: ${CIRCLE_JOB}"
+        integrations.slack.sendMessageToChannel "bashlibs" "Terraform destroy successfully executed on job: ${CIRCLE_JOB}"
+
+    elif [[ "$(git log --format=oneline -n 1 ${CIRCLE_SHA1} | grep -E "\[${CIRCLE_COMMIT_APPLY}\]")" ]] \
+        || [[ -n ${loc} ]]; then
+    
+        echoInfo "Terraform Apply flag detected!... [Updating GCP Resources]"
+        terraform.init_gcp "${terraform_path}" "${GCLOUD_PROJECT_BUCKET_NAME}" "terraform"
+        terraform.apply "${terraform_path}"
+
+        integrations.telegram.sendMessage "${TELEGRAM_NOTIFICATION_ID}" "Terraform apply successfully executed on job: ${CIRCLE_JOB}"
+        integrations.slack.sendMessageToChannel "bashlibs" "Terraform apply successfully executed on job: ${CIRCLE_JOB}"
+    
+    else
+    
+        echoInfo "Terraform will not be executed!"
+        integrations.telegram.sendMessage "${TELEGRAM_NOTIFICATION_ID}" "Terraform was not executed on job: ${CIRCLE_JOB}"
+        integrations.slack.sendMessageToChannel "bashlibs" "Terraform was not executed on job: ${CIRCLE_JOB}"
+    
+    fi
+}
