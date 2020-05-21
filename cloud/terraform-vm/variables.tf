da650d6ae43908242b6d0b351d8978a64ae8f5cc
rodolfotiago <rodolfotiago@gmail.com>
2020-05-21 20:22:47 -0300

    [tf-plan]

--- cloud/terraform-vm/variables.tf
+++ cloud/terraform-vm/variables.tf
@@ -7,6 +7,9 @@ variable "machine_type" { }
 variable "cluster_count" { }
 variable "cluster_name" { }
 variable "ssd_name" { }
+variable "compute_instance_environment" { }
+variable "ubuntu_image" { }
+variable "startup_script" { }
 
 variable "MONGO_ATLAS_STRING" { }
 variable "JWT_KEY" { }
