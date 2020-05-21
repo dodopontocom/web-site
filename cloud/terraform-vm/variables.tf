ac040105f14392a674883ac71c94176c8272eeb3
rodolfotiago <rodolfotiago@gmail.com>
2020-05-21 20:16:50 -0300

    [tf-plan]

--- cloud/terraform-vm/variables.tf
+++ cloud/terraform-vm/variables.tf
@@ -6,6 +6,7 @@ variable "gcp_bucket" { }
 variable "machine_type" { }
 variable "cluster_count" { }
 variable "cluster_name" { }
+variable "ssd_name" { }
 
 variable "MONGO_ATLAS_STRING" { }
 variable "JWT_KEY" { }
