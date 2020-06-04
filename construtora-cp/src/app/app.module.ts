d0372e013e0e548140429f4407e13c20e6ae4083
rodolfotiago <rodolfotiago@gmail.com>
2020-06-04 13:38:08 -0300

    [skip ci] auth into module

--- construtora-cp/src/app/app.module.ts
+++ construtora-cp/src/app/app.module.ts
@@ -5,19 +5,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 
-import { FormsModule } from "@angular/forms";
-
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from "./auth/auth-interceptor";
 import { ErrorInterceptor } from "./error-interceptor";
 import { ErrorComponent } from "./error/error.component";
 import { AngularMaterialModule } from './angular-material.module';
 import { PostsModule } from './posts/posts.module';
+import { AuthModule } from './auth/auth.module';
 
 
 
@@ -25,8 +22,6 @@ import { PostsModule } from './posts/posts.module';
   declarations: [
     AppComponent,
     HeaderComponent,
-    LoginComponent,
-    SignupComponent,
     ErrorComponent
   ],
   imports: [
@@ -35,9 +30,9 @@ import { PostsModule } from './posts/posts.module';
     HttpClientModule,
     RouterModule,
     AppRoutingModule,
-    FormsModule,
     AngularMaterialModule,
-    PostsModule
+    PostsModule,
+    AuthModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
