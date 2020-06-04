2bf96cf7086a19ef6acc1c1f81ad02ec45b0dae8
rodolfotiago <rodolfotiago@gmail.com>
2020-06-04 11:37:02 -0300

    [skip ci] ng module

--- construtora-cp/src/app/app.module.ts
+++ construtora-cp/src/app/app.module.ts
@@ -9,16 +9,6 @@ import { PostListComponent } from './posts/post-list/post-list.component';
 
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatDialogModule } from '@angular/material/dialog';
-
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -28,6 +18,7 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from "./auth/auth-interceptor";
 import { ErrorInterceptor } from "./error-interceptor";
 import { ErrorComponent } from "./error/error.component";
+import { AngularMaterialModule } from './angular-material.module';
 
 
 
@@ -44,20 +35,12 @@ import { ErrorComponent } from "./error/error.component";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule,
     HttpClientModule,
-    MatProgressSpinnerModule,
     RouterModule,
     AppRoutingModule,
-    MatIconModule,
     ReactiveFormsModule,
     FormsModule,
-    MatPaginatorModule,
-    MatDialogModule
+    AngularMaterialModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
