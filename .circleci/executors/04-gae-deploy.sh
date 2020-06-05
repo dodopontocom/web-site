a4a6876838f1b58835d82b98e84166d9148cba1b
rodolfotiago <rodolfotiago@gmail.com>
2020-06-05 14:17:57 -0300

    [appengine]

---
+++ .circleci/executors/04-gae-deploy.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#
+executor.GAE_Deploy_App() {
+
+    local loc destroy
+    loc=$1
+    destroy=$2
+
+    checkVars loc || echo ${DODRONES_GCP_MY_LABS_SA} > ${GCLOUD_JSON_KEY_PATH}
+    
+    # Import required lib
+    do.use gcp.auth
+    do.use gae
+    
+    if [[ "$(git log --format=oneline -n 1 ${CIRCLE_SHA1} | grep -E "\[${CIRCLE_COMMIT_GAE}\]")" ]] \
+        || [[ -n ${loc} ]] && [[ -z ${destroy} ]]; then
+
+        echoInfo "Deploying Application in Google App Engine"
+        gcp.auth.useSA ${GOOGLE_APPLICATION_CREDENTIALS}
+        
+        GAE_DEPLOYMENT_VERSION=""
+        if [[ "${CIRCLE_BRANCH}" -eq "develop" ]]; then
+            GAE_DEPLOYMENT_VERSION="${CIRCLE_BRANCH}"
+        fi
+        if [[ -z ${K8S_DEPLOYMENT_TAG} ]] && [[ "${CIRCLE_BRANCH}" -ne "master" ]]; then
+            GAE_DEPLOYMENT_VERSION="build-${CIRCLE_BUILD_NUM}"
+        fi
+
+        echo "MONGO_ATLAS_STRING=\"${MONGO_ATLAS_STRING}\"" > ${CIRCLE_WORKING_DIRECTORY}/construtora-cp/backend/.env
+        echo "JWT_KEY=\"${JWT_KEY}\"" >> ${CIRCLE_WORKING_DIRECTORY}/construtora-cp/backend/.env
+
+        gcp.gae.deploy "${CIRCLE_WORKING_DIRECTORY}/construtora-cp/backend/" "version=${GAE_DEPLOYMENT_VERSION}"
+
+        integrations.telegram.sendMessage "${TELEGRAM_NOTIFICATION_ID}" "Application deployment done on job: ${CIRCLE_JOB}"
+        integrations.telegram.sendMessage "${TELEGRAM_NOTIFICATION_ID}" "You can access the App here: ${app_url}"
+        integrations.slack.sendMessageToChannel "bashlibs" "Application deployment done on job: ${CIRCLE_JOB}"
+        integrations.slack.sendMessageToChannel "bashlibs" "You can access the App here: ${app_url}"
+    
+    else
+    
+        echoInfo "Skipping this step... no flag is set"
+        integrations.telegram.sendMessage "${TELEGRAM_NOTIFICATION_ID}" "Application deployment was skipped on job: ${CIRCLE_JOB}"
+        integrations.slack.sendMessageToChannel "bashlibs" "Application deployment was skipped on job: ${CIRCLE_JOB}"
+    
+    fi
+}
