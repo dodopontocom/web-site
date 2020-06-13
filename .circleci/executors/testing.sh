26236f18f7bf332ffb2c6109ffbe43ba4079dccc
rodolfotiago <rodolfotiago@gmail.com>
2020-06-12 23:20:58 -0300

    go

--- .circleci/executors/testing.sh
+++ .circleci/executors/testing.sh
@@ -3,10 +3,10 @@
 
 executor.Circle_Testing() {
     echoInfo "This is a circle testing message"
-    PACKAGE_VERSION=$(cat ${ROOT_DIT}/${APP_NAME}/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+    PACKAGE_VERSION=$(cat ${ROOT_DIR}/${APP_NAME}/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
     echo ${PACKAGE_VERSION}
-    helper.npm_version ${CIRCLE_BRANCH} ${ROOT_DIT}/${APP_NAME}/
-    PACKAGE_VERSION=$(cat ${ROOT_DIT}/${APP_NAME}/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+    helper.npm_version ${CIRCLE_BRANCH} ${ROOT_DIR}/${APP_NAME}/
+    PACKAGE_VERSION=$(cat ${ROOT_DIR}/${APP_NAME}/package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
     echo ${PACKAGE_VERSION}
     integrations.slack.sendMessageToChannel "bashlibs" "Hello from testing job: ${CIRCLE_JOB}"
 }
